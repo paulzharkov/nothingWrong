efeabe93e7ca41bf0c8fdbc6af4d45b100d092ff
Igor <shevcovigor3085@yandex.ru>
2020-12-18 14:47:57 +0300

    1

--- server/app.js
+++ server/app.js
@@ -30,14 +30,14 @@ io.on("connection", socket => {
 
 const PORT = process.env.PORT || 3001;
 dbConnect();
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}))
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   credentials: true
+// }))
 
 app.use(
   session({
