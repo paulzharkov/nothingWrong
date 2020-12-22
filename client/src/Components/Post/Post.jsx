e3599288e588656ddb2c24a5290efa465248800f
Pavel_Zharkov <p.zharkov9@gmail.com>
2020-12-22 21:16:43 +0300

    cardsDullUpdate

--- client/src/Components/Post/Post.jsx
+++ client/src/Components/Post/Post.jsx
@@ -8,22 +8,27 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import sticker from './3.jpeg'
+import imgFamily from './family.jpeg'
+import imgHome from './home.jpeg'
+import imgMoney from './money.jpeg'
+import imgShout from './shout.jpeg'
+
 
 const useStyles = makeStyles({
   root: {
-    // display: 'flex',
+    // border: '1px solid',
+    boxShadow: '0 4px 0 #67a3a3',
+    marginBottom: '7px',
   },
   postImage: {
     display: 'flex',
     flexDirection: 'row',
-    // width: '100px',
   },
   buttons: {
     display: 'flex',
     alignSelf: 'flex-end',
   },
-  image: {
+  imageStyle: {
     width: '100%',
     maxWidth: '180px'
   }
@@ -52,40 +57,48 @@ function Post({ category, reason, solve, status, rating, state, offender, likes,
     history.push(`/lenta/${id}`)
   }
 
-  return (
-    <Card className={classes.root}>
-      <div className={classes.postImage}>
-      
-        <div>
-          <CardContent>
-            <Typography gutterBottom variant="subtitle1" component="h2">Категория: {category}</Typography>
-            <Typography variant="body2" color="textSecondary" component="p">Причина: {reason}</Typography>
-            <Typography variant="body2" color="textSecondary" component="p">Пути Решения: {solve}</Typography>
-            <Typography variant="body2" color="textSecondary" component="p">Статус: {status}</Typography>
-            <Typography variant="body2" color="textSecondary" component="p">Уровень: {rating}</Typography>
-            <Typography variant="body2" color="textSecondary" component="p">Обидчик: {offenderName}</Typography>
-            <Typography variant="body2" color="textSecondary" component="p">Дата создания: {date}</Typography>
-          </CardContent>
-        </div>
-        <div className={classes.image}>
-        <CardMedia 
-          component="img"
-          alt="Card image"
-          image={sticker}
-          title="Card image"
-        />
-        </div>
-      </div>
-      <CardActions>
-        <div className={classes.buttons}>
-          <Button size="large" color="primary" onClick={handlerComments}>💬{comments.length}</Button>
-          <Button size="large" color="primary" onClick={(e) => { e.preventDefault(); window.location.href = '/likes'; }}>❤️️{likes.length}</Button>
-          {offender ? <Button size="large" color="primary" onClick={handlerDelete}>Удалить</Button> : null}
-          {state ? <Button size="large" color="primary" onClick={handlerChatPrivat}>Обсудить в чате</Button> : null}
-        </div>
-      </CardActions>
-    </Card>
+  let picture = imgShout
 
+
+  return (
+    <>
+      <div style={{ display: 'none' }}>
+        {category === 'Воспитательная' ? picture = imgFamily : null}
+        {category === 'Финансовая' ? picture = imgMoney : null}
+        {category === 'Бытовая' ? picture = imgHome : null}
+      </div>
+
+      <Card className={classes.root}>
+        <div className={classes.postImage}>
+          <div>
+            <CardContent>
+              <Typography gutterBottom variant="subtitle1" component="h2">Категория: {category}</Typography>
+              <Typography variant="body2" color="textSecondary" component="p">Причина: {reason}</Typography>
+              <Typography variant="body2" color="textSecondary" component="p">Пути Решения: {solve}</Typography>
+              <Typography variant="body2" color="textSecondary" component="p">Статус: {status}</Typography>
+              <Typography variant="body2" color="textSecondary" component="p">Уровень: {rating}</Typography>
+              <Typography variant="body2" color="textSecondary" component="p">Обидчик: {offenderName}</Typography>
+              <Typography variant="body2" color="textSecondary" component="p">Дата создания: {date}</Typography>
+            </CardContent>
+          </div>
+          <div className={classes.imageStyle}>
+            <CardMedia
+              component="img"
+              alt="Card"
+              image={picture}
+            />
+          </div>
+        </div>
+        <CardActions>
+          <div className={classes.buttons}>
+            <Button size="large" color="primary" onClick={handlerComments}>💬{comments.length}</Button>
+            <Button size="large" color="primary" onClick={(e) => { e.preventDefault(); window.location.href = '/likes'; }}>❤️️{likes.length}</Button>
+            {offender ? <Button size="large" color="primary" onClick={handlerDelete}>Удалить</Button> : null}
+            {state ? <Button size="large" color="primary" onClick={handlerChatPrivat}>Обсудить в чате</Button> : null}
+          </div>
+        </CardActions>
+      </Card>
+    </>
 
   )
 }
