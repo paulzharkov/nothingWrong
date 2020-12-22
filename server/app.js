b3156e6f8a784b06bccac4171760af05e2a35089
Igor <shevcovigor3085@yandex.ru>
2020-12-22 19:01:43 +0300

    icon_start

--- server/app.js
+++ server/app.js
@@ -10,11 +10,13 @@ const dbConnect = require('./src/config/db');
 const http = require("http");
 const cors = require('cors');
 const User = require('../server/src/models/user.model')
+const Post = require('../server/src/models/post.model')
 
 const app = express();
 
 const server = http.createServer(app);
 const socket = require("socket.io");
+
 
 
 const io = socket(server);
@@ -49,7 +51,7 @@ io.use((socket, next) => {
   // connections, as 'socket.request.res' will be undefined in that case
 });
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
 
   const session = socket.request.session;
 
@@ -58,39 +60,50 @@ io.on('connection', (socket) => {
     session.save();
     socket.emit("your id", socket.id);
 
+    await User.findOneAndUpdate({_id: session.user.id}, {socketID: socket.id})
+
     socket.on("send message", body => {
+      console.log(body)
       io.emit("message", body)
     })
 
     socket.on("private message", async body => {
 
-      console.log(body.user);
-      console.log('-----> server', body.idOne);
-      const myName = await User.findOne({ login: body.user })
+      console.log('body', body);
+      io.to(socket.id).emit("hey", "I just met you");
+      // const post = await Post.findOne({ _id: body.idOne})
+      // post.sms.push({ body: body.body, id: body.id })
+      // await post.save()
 
-      function idToSrting(arr) {
-        return arr.map((e) => {
-          return e.toString()
-          })
-      }
-      const yourName = await User.findOne({ login: body.offenderId})
-      const myNameMyHurt = idToSrting(myName.myHurt)
-      const myNameToMeHurt = idToSrting(myName.toMeHurt)
-      const yourNameMyHurt = idToSrting(yourName.myHurt)
-      const yourNameToMeHurt = idToSrting(yourName.toMeHurt)
+      // const myName = await User.findOne({ _id: body.userId })
+      // const yourName = await User.findOne({ _id: body.authorId})
 
-      const userHurtIdMy = myNameMyHurt.find((e) => e === body.idOne)
+      // function idToSrting(arr) {
+      //   return arr.map((e) => {
+      //     return e.toString()
+      //     })
+      // }
+      // const myNameMyHurt = idToSrting(myName.myHurt)
+      // const myNameToMeHurt = idToSrting(myName.toMeHurt)
+      // const yourNameMyHurt = idToSrting(yourName.myHurt)
+      // const yourNameToMeHurt = idToSrting(yourName.toMeHurt)
+
+      // const userHurtIdMy = myNameMyHurt.find((e) => e === body.idOne)
 
 
+      // console.log('yourName', yourName);
+      // console.log('myName', myName);
+      // // const body2 = [{}, {}]
 
-      const userHurtIdApponent = myNameToMeHurt.find((e) => e === body.idOne)
-      const apponentHurtIdMy = yourNameMyHurt.find((e) => e === body.idOne)
-      const apponentHurtIdUser = yourNameToMeHurt.find((e) => e === body.idOne)
 
-      if (userHurtIdMy === apponentHurtIdUser || userHurtIdApponent === apponentHurtIdMy) {
-        // io.emit("private message", body)
-        io.emit(`${body.idOne}`, body)
-      }
+      // const userHurtIdApponent = myNameToMeHurt.find((e) => e === body.idOne)
+      // const apponentHurtIdMy = yourNameMyHurt.find((e) => e === body.idOne)
+      // const apponentHurtIdUser = yourNameToMeHurt.find((e) => e === body.idOne)
+      
+      // io.emit("private message", post.sms)
+      // if (userHurtIdMy === apponentHurtIdUser || userHurtIdApponent === apponentHurtIdMy) {
+      //   io.emit(`${body.idOne}`, body)
+      // }
     })
   }
 });
