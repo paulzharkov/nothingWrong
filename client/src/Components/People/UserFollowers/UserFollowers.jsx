1313e18d0ab22ff29bd743076e55f70d4377ca9b
Pavel_Zharkov <p.zharkov9@gmail.com>
2020-12-23 15:48:05 +0300

    peopleList

--- client/src/Components/People/UserFollowers/UserFollowers.jsx
+++ client/src/Components/People/UserFollowers/UserFollowers.jsx
@@ -1,5 +1,9 @@
 import { useDispatch, useSelector } from "react-redux"
 import * as AC from '../../../redux/creators/usersList'
+import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 
 function UserFollowers({ login, email, id }) {
@@ -10,11 +14,30 @@ function UserFollowers({ login, email, id }) {
     dispatch(AC.unSubscribeThunk(id))
   }
 
+  const RandomButton = withStyles(() => ({
+    root: {
+      backgroundColor: '#FFCCA1',
+      color: '#216A6A',
+      width: '50vw',
+      height: '3vh',
+      marginTop: '10px',
+      marginBottom: '10px',
+      boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
+    },
+  }))(Button);
+
   return (
     <div>
-      <div>Логин: {login}</div>
-      <div>Email: {email}</div>
-      <button type="button" onClick={unSubscribeHandler}>Отписаться</button>
+      <Typography variant="h6" component="h1">Логин: {login}</Typography>
+      <Typography variant="body1" color="textPrimary" component="p">Email: {email}</Typography>
+
+      <RandomButton
+        onClick={unSubscribeHandler}
+        variant="outlined"
+        endIcon={<Icon style={{ fontSize: 20 }}>clear</Icon>}
+      >
+        Отписаться
+        </RandomButton>
     </div>
   )
 }
